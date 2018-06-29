f54a1ed2292fd7466edf936b39cd063fd58b9e87
ZhAYinG <8896093+zhaying@users.noreply.github.com>
2018-06-29 04:42:22 -0400

    build system redo

--- webpack.common.js
+++ webpack.common.js
@@ -6,15 +6,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
   entry: './client/js/index.js'
-  // app: './client/js/components/App.js',
-  // contactus: './client/js/components/views/pages/ContactUs.js',
-  // customers: './client/js/components/views/pages/Customers.js',
-  // home: './client/js/components/views/pages/Home.js',
-  // login: './client/js/components/views/pages/Login.js',
-  // pricing: './client/js/components/views/pages/Pricing.js',
-  // products: './client/js/components/views/pages/Products.js',
-  // support: './client/js/components/views/pages/Support.js',
-  // usecases: './client/js/components/views/pages/UseCases.js',
   },
   output: {
     path: path.resolve(__dirname, '.tmp/public'),
@@ -41,19 +32,3 @@ module.exports = {
   },
   optimization: { splitChunks: {chunks: 'all'} }
 };
-// module.exports = {
-//    entry: {
-//      app: './src/index.js'
-//    },
-//    output: {
-//      filename: '[name].bundle.js',
-//      path: path.resolve(__dirname, '.tmp/public')
-//    },
-//    plugins: [
-//      new CleanWebpackPlugin(['dist']),
-//      new HtmlWebpackPlugin({
-//        title: 'Production'
-//      })
-//    ]
-//
-// };
