c1ac966a2e8169ba6236b5f92a61824ed30a3efd
Lauren Ventura <laurenfventura@gmail.com>
2018-07-05 18:39:13 -0400

    added custom colors to MUI theme

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import { orange } from '@material-ui/core/colors'
 import { createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
 import '../css/style.css';
 
@@ -10,13 +8,20 @@ import App from './components/App';
 const theme = createMuiTheme({
     palette: {
       primary: {
-        light: orange[200], // same as '#FFCC80',
-        main: '#FB8C00', // same as orange[600]
-        dark: '#EF6C00',
-        contrastText: 'rgb(0,0,0)'
-      }
-    }
-  })
+        // light: will be calculated from palette.primary.main,
+        main: '#00d49d',
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contast with palette.primary.main
+      },
+      secondary: {
+        // light: will be calculated from palette.primary.main,
+        main: '#0084b5',
+        // dark: will be calculated from palette.secondary.main,
+        contrastText: '#ffcc00',
+      },
+      // error: will use the default color
+    },
+  });
 
 render(
     <MuiThemeProvider theme={theme}>
