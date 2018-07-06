a1d9fbf225e3f483c733352349669b00b45331b7
Lauren Ventura <laurenfventura@gmail.com>
2018-07-06 13:46:23 -0400

    added paper element to login component (spread error)

--- client/js/components/views/pages/Login.js
+++ client/js/components/views/pages/Login.js
@@ -1,16 +1,57 @@
 import React from 'react';
-// import { Jumbotron, Button } from 'reactstrap';
-import { Button } from '@material-ui/core/Button';
-import LoginForm from '../forms/LoginForm';
-const Login = () => (
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
+
+function Login(props) {
+  const { classes } = props;
+
+  return (
     <div>
-      <Jumbotron>
-        <h1 className="display-3">Login</h1>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="headline" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
         <p className="lead">
           <Button variant="contained" color="primary">Login</Button>
         </p>
-      </Jumbotron>
+      </Paper>
     </div>
-);
+  );
+}
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
+
+
+// import React from 'react';
+// // import { Jumbotron, Button } from 'reactstrap';
+// import { Button } from '@material-ui/core/Button';
+// import LoginForm from '../forms/LoginForm';
+// const Login = () => (
+//     <div>
+//       <Jumbotron>
+//         <h1 className="display-3">Login</h1>
+//         <p className="lead">
+//           <Button variant="contained" color="primary">Login</Button>
+//         </p>
+//       </Jumbotron>
+//     </div>
+// );
+
+// export default Login;
