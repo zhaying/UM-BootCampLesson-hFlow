642dd3c050d03ce5addc3e3897f3833a56912a97
Lauren Ventura <laurenfventura@gmail.com>
2018-07-07 11:06:47 -0400

    babel loader error

--- client/js/components/views/Login/Login.js
+++ client/js/components/views/Login/Login.js
@@ -34,31 +34,10 @@ const styles = theme => ({
     }
 });
 
-// const currencies = [
-//   {
-//     value: 'USD',
-//     label: '$',
-//   },
-//   {
-//     value: 'EUR',
-//     label: '€',
-//   },
-//   {
-//     value: 'BTC',
-//     label: '฿',
-//   },
-//   {
-//     value: 'JPY',
-//     label: '¥',
-//   },
-// ];
-
 class TextFields extends React.Component {
   state = {
     name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
+    age: ''
   };
 
   handleChange = name => event => {
@@ -106,58 +85,3 @@ TextFields.propTypes = {
 };
 
 export default withStyles(styles)(TextFields);
-
-
-// const styles = theme => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   paper: {
-//     padding: theme.spacing.unit * 2,
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   },
-//   topMargin: {
-//     marginTop: '80px'
-//   }
-// });
-
-// function Login(props) {
-//   const { classes } = props;
-
-//   return (
-//     <div className={classes.root}>
-//       <Grid container spacing={24} className={classes.topMargin}>
-//         <Grid item xs={12}>
-//           <Paper className={classes.paper}>xs=12</Paper>
-//         </Grid>
-//         <Grid item xs={12} sm={6}>
-//           <Paper className={classes.paper}>xs=12 sm=6</Paper>
-//         </Grid>
-//         <Grid item xs={12} sm={6}>
-//           <Paper className={classes.paper}>xs=12 sm=6</Paper>
-//         </Grid>
-//         <Grid item xs={6} sm={3}>
-//           <Paper className={classes.paper}>xs=6 sm=3</Paper>
-//         </Grid>
-//         <Grid item xs={6} sm={3}>
-//           <Paper className={classes.paper}>xs=6 sm=3</Paper>
-//         </Grid>
-//         <Grid item xs={6} sm={3}>
-//           <Paper className={classes.paper}>xs=6 sm=3</Paper>
-//         </Grid>
-//         <Grid item xs={6} sm={3}>
-//           <Paper className={classes.paper}>xs=6 sm=3</Paper>
-//         </Grid>
-//       </Grid>
-//     </div>
-//   );
-
-
-// Login.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
-
-// }
-
-// export default withStyles(styles)(Login);
