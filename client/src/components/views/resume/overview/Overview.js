33c9504cd2b5e37c501994e4e47526a2e755c4b6
Lauren Ventura <laurenfventura@gmail.com>
2018-07-08 16:23:38 -0400

    adding components to resume and added speed dial component

--- client/src/components/views/resume/overview/Overview.js
+++ client/src/components/views/resume/overview/Overview.js
@@ -3,48 +3,56 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import OpenIconSpeedDial from '../speedDial/SpeedDial';
+
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  topMargin: {
-    marginTop: '80px'
-  }
+    root: {
+        flexGrow: 1,
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit * 12,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    menu: {
+        width: 200,
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        width: 400
+    }
 });
 
-function Home(props) {
-  const { classes } = props;
+class Overview extends React.Component {
+  
 
-  return(
-    <div className={classes.root}>
-      <Grid container spacing={24} className={classes.topMargin}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <h2>Orchestrate your team to move towards order and away from chaos...</h2>
-            <p className="lead">
-              See how hFlow stacks up.
-            </p>
-            <p className="lead">
-              <Button variant="contained" color="primary">FIND OUT NOW</Button>
-            </p>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+  render() {
+    const { classes } = this.props;
 
-  // Home.propTypes = {
-  //   classes: PropTypes.object.isRequired,
-  // };
-
+    return (
+        <div className={classes.root}>
+            <Grid container alignItems={'center'} spacing={24}>
+                <Grid item xs={12}>
+                    <h1>Overview</h1>
+                    <p className="lead">In non nulla purus. Nulla dictum nisl lacus, quis semper dui gravida ut. Sed eu metus augue. Donec augue dui, tempor in ante a, elementum laoreet nisi. Etiam nec sapien id magna placerat gravida. Cras id pulvinar mauris. Etiam id efficitur nunc. Praesent laoreet blandit nunc, ac consectetur massa. Nam massa tellus, lacinia non sem at, pretium euismod dui. Nam pellentesque erat nec auctor faucibus. Fusce convallis pulvinar magna, eget dapibus leo laoreet ut. Mauris eu pretium leo. Curabitur non tincidunt lectus.</p>
+                    <OpenIconSpeedDial />
+                </Grid>
+            </Grid>
+        </div>
+    );
+  }
 }
 
-export default withStyles(styles)(Home);
+Overview.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Overview);
