54f304c640f982d67922b77783d158b6d75b74e2
Lauren Ventura <laurenfventura@gmail.com>
2018-07-09 11:07:02 -0400

    dash

---
+++ client/src/components/views/dashboard/Dashboard.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import SimpleCard from '../card/Card';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  topMargin: {
+    margin: '5px',
+  },
+  cMargin: {
+    margin: '30px',
+  }
+});
+
+function Dashboard(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container className={classes.cMargin} spacing={24}>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+        <Grid container className={classes.topMargin} justify="center" spacing={24}>
+            <Grid item xs={6}>
+              <SimpleCard />
+            </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Dashboard);
