e18771b7e843b4eb5f1241ece6d6ea46be9fdb44
Lauren Ventura <laurenfventura@gmail.com>
2018-07-09 16:28:49 -0400

    dashboard?

--- client/src/components/views/navbarDash/NavbarDash.js
+++ client/src/components/views/navbarDash/NavbarDash.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Hidden from '@material-ui/core/Hidden';
 import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import Dashboard from '../dashboard/Dashboard';
 // import { mailFolderListItems, otherMailFolderListItems } from './tileData';
 
 const drawerWidth = 240;
@@ -17,9 +18,7 @@ const drawerWidth = 240;
 const styles = theme => ({
   root: {
     flexGrow: 1,
-  },
-  appFrame: {
-    height: 'vh',
+    height: '100%',
     zIndex: 1,
     overflow: 'hidden',
     position: 'relative',
@@ -27,21 +26,25 @@ const styles = theme => ({
     width: '100%',
   },
   appBar: {
-    backgroundColor: '#FAFAFA',
-    borderShadow: 'none',
-    width: `calc(100% - ${drawerWidth}px)`,
-  },
-  'appBar-left': {
+    position: 'absolute',
+    // backgroundColor: '#FAFAFA',
     marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
   },
-  'appBar-right': {
-    marginRight: drawerWidth,
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
   },
   toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
@@ -51,75 +54,74 @@ const styles = theme => ({
 
 class DashAppBar extends React.Component {
   state = {
-    anchor: 'left',
+    mobileOpen: false,
   };
 
-  handleChange = event => {
-    this.setState({
-      anchor: event.target.value,
-    });
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
 
   render() {
-    const { classes } = this.props;
-    const { anchor } = this.state;
+    const { classes, theme } = this.props;
 
     const drawer = (
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor={anchor}
-      >
+      <div>
         <div className={classes.toolbar} />
         <Divider />
         {/* <List>{mailFolderListItems}</List> */}
         <Divider />
         {/* <List>{otherMailFolderListItems}</List> */}
-      </Drawer>
+      </div>
     );
-
-    let before = null;
-    let after = null;
-
-    if (anchor === 'left') {
-      before = drawer;
-    } else {
-      after = drawer;
-    }
 
     return (
       <div className={classes.root}>
-        <TextField
-          id="permanent-anchor"
-          select
-          label="Anchor"
-          value={anchor}
-          onChange={this.handleChange}
-          margin="normal"
-        >
-          <MenuItem value="left">left</MenuItem>
-          <MenuItem value="right">right</MenuItem>
-        </TextField>
-        <div className={classes.appFrame}>
-          <AppBar
-            position="absolute"
-            className={classNames(classes.appBar, classes[`appBar-${anchor}`])}
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
           >
-            <Toolbar>
-              <Typography variant="title" color="inherit" noWrap>
-                Permanent drawer
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          {before}
-          <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Typography>{'You think water moves fast? You should see ice.'}</Typography>
-          </main>
-          {after}
-        </div>
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Dashboard />
+        </main>
       </div>
     );
   }
@@ -127,6 +129,7 @@ class DashAppBar extends React.Component {
 
 DashAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(DashAppBar);
+export default withStyles(styles, { withTheme: true })(DashAppBar);
