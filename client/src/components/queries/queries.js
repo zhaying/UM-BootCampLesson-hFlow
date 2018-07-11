2fc40ceada6a1202f54552ccbdac83587edd74ea
JackieP <jqbluew@mac.com>
2018-07-10 20:56:19 -0400

    started added graphql

---
+++ client/src/components/queries/queries.js
@@ -0,0 +1,51 @@
+import {gql} from 'apollo-boost';
+
+
+const getExecutivesQuery = gql`
+  {
+    executives{
+      name,
+      title,
+      preferences,
+      currentEvent,
+      id
+    }
+  }
+`
+
+/*const getEventsQuery = gql`
+  {
+    events{
+      meeting,
+      eventName,
+      dates,
+      resumeType,
+      executiveId,
+      preferenceId
+    }
+  }
+`
+*/
+
+const addExecutiveMutation = gql`
+  mutation AddExecutive($name:String!, $title:String!, $preferences:String!){
+    addExecutive(name:$name, title:$title, preferences:$preferences){
+      name
+      title
+      preferences
+    }
+  }
+`
+
+const getExecutiveQuery = gql`
+  query($id:ID){
+      executive(id:$id){
+        name
+        title
+        preferences
+    }
+  }
+`
+
+
+export{getExecutivesQuery, addExecutiveMutation, getExecutiveQuery};
--- schema/schema.js
+++ schema/schema.js
@@ -1,33 +1,18 @@
 const graphql = require ('graphql');
 const _ = require ('lodash');
+const Executive = require('../models/executive');
+const Event = require('../models/event');
+const Preference = require('../models/preference');
+
 const {
   GraphQLObjectType,
-  GraphQLList,
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
-
-//dummy data
-var executives = [
-  {name:'Mary Barra', title:'CEO', preferences:'Allergic to gluten', currentEvent:'4', id:'1'},
-  {name:'Wrigley Payano', title:'CEO', preferences:'Loves carrots and kills lizards', currentEvent:'4', id:'2'},
-  {name:'John Smith', title:'CFO', preferences:'Loves Pinot Noir from Beune', currentEvent:'5', id:'3'},
-];
-
-var preferences = [
-  {allergy:'none', food:'Cupckes', preferences:'Allergic to gluten', id:'1'},
-  {name:'Wrigley Payano', title:'CEO', preferences:'Loves carrots and kills lizards', id:'2'},
-  {name:'John Smith', title:'CFO', preferences:'Loves Pinot Noir from Beune', id:'3'},
-];
-
-var events = [
-  {organizationName:'Cengage', eventName:'Engage', dates:'Sunday December 12, 2018 through Thursday December 16, 2018', resumeType:'',id:'4'},
-  {organizationName:'Cisco', eventName:'Cisco Live', dates:'Sunday October 12, 2018 through Thursday October 16, 2018', resumeType:'',id:'5'},
-  {organizationName:'MicroSoft', eventName:'Azure Live', dates:'Sunday November 12, 2018 through Thursday November 16, 2018', resumeType:'',id:'6'}
-];
-
 
 const ExecutiveType = new GraphQLObjectType({
     name:'Executive',
@@ -36,7 +21,22 @@ const ExecutiveType = new GraphQLObjectType({
       name:{type:GraphQLString},
       title:{type:GraphQLString},
       preferences:{type:GraphQLString},
-      currentEvent:{type:GraphQLString}
+      currentEvent:{type:GraphQLString},
+      preference:{
+        type:PreferenceType,
+        resolve(parent, args){
+          //console.log(parent)
+          //return _.find(preferences, {id:parent.preferenceId})
+          return Preference.findById(parent.preferenceId);
+        }
+      },
+      events: {
+        type: new GraphQLList(EventType),
+        resolve(parent, args){
+          //return _.filter(events,{executiveId:parent.id});
+          return Event.find({executiveId:parent.id});
+        }
+      }
     })
 });
 
@@ -44,28 +44,33 @@ const PreferenceType = new GraphQLObjectType({
     name:'Preference',
     fields:()=>({
       id:{type:GraphQLID},
-      name:{type:GraphQLString},
-      title:{type:GraphQLString},
-      preferences:{type:GraphQLString}
+      allergy:{type:GraphQLString},
+      amenity:{type:GraphQLString},
+      favorites:{type:GraphQLString},
+      executiveId: {type:GraphQLID},
+      eventId: {type:GraphQLID}
     })
 });
 
 const EventType = new GraphQLObjectType({
-    name:'event',
+    name:'Event',
     fields:()=>({
       id:{type:GraphQLID},
-      organizationName:{type:GraphQLString},
+      meeting:{type:GraphQLString},
       eventName:{type:GraphQLString},
       dates:{type:GraphQLString},
       resumeType:{type:GraphQLString},
-      executives: {
-        type: new GraphQLList(ExecutiveType),
-        resolve(parent, args) {
-          //return _.filter(books, {authorid: parent.id})
-          return _.filter(executives,{currentEvent:parent.id});
+      executiveId: {type:GraphQLID},
+      preferenceId: {type:GraphQLID}
+    /*  Executive:{
+        type:ExecutiveType,
+        resolve(parent, args){
+          //code to get data from db/other source
+          console.log('eventparent=',parent.id);
+          return _.filter(executives, { 'currentEvent': parent.id});
         }
 
-      }
+      }*/
     })
 });
 const RootQuery = new GraphQLObjectType({
@@ -76,26 +81,146 @@ const RootQuery = new GraphQLObjectType({
       args:{id:{type:GraphQLID}},
       resolve(parent, args){
         //code to get data from db/other source
-        return _.find(executives, {id:args.id});
+        //return _.find(executives, {id:args.id});
+        return Executive.findById(args.id);
       }
     },
     preference: {
       type: PreferenceType,
       args:{id:{type:GraphQLID}},
       resolve(parent, args){
-        return _.find(preferences, {id:args.id});
+        //return _.find(preferences, {id:args.id});
+        return Preference.findById(args.id);
       }
     },
     event:{
      type: EventType,
      args:{id:{type:GraphQLID}},
      resolve(parent, args){
-       return _.find(events, {id:args.id});
+       //return _.find(events, {id:args.id});
+       return Event.findById(args.id);
      }
+   },
+   executives:{
+     type:new GraphQLList(ExecutiveType),
+     resolve(parent, args){
+       //return executives
+       return Executive.find({});
+     }
+   },
+   preferences: {
+     type: new GraphQLList(PreferenceType),
+     resolve(parent, args){
+       //return preferences
+       return Preference.find({});
+     }
+   },
+   events: {
+     type: new GraphQLList(EventType),
+     resolve(parent, args){
+      // return events
+      return Event.find({});
+     }
+   }
+  }
+})
+
+const Mutation = new GraphQLObjectType({
+  name:'Mutation',
+  fields:{
+    addExecutive: {
+      type: ExecutiveType,
+      args:{
+        name:{type: new GraphQLNonNull(GraphQLString)},
+        title: {type: new GraphQLNonNull(GraphQLString)},
+        preferences: {type: new GraphQLNonNull(GraphQLString)},
+        currentEvent: {type:GraphQLString},
+        preferenceId:{type:GraphQLID},
+        eventId:{type:GraphQLID}
+      },
+      resolve(parent, args){
+        let executive = new Executive({
+          name: args.name,
+          title: args.title,
+          preferences: args.preferences,
+          currentEvent: args.currentEvent,
+          preferenceId: args.preferenceId,
+          eventId: args.eventId
+        });
+        return executive.save();
+      }
+    },
+    addEvent: {
+      type:EventType,
+      args:{
+        meeting: {type:new GraphQLNonNull(GraphQLString)},
+        eventName: {type:new GraphQLNonNull(GraphQLString)},
+        dates: {type:GraphQLString},
+        resumeType: {type:GraphQLString},
+        executiveId: {type:GraphQLID},
+        preferenceId: {type:GraphQLID}
+      },
+      resolve(parent, args){
+        let event = new Event({
+          meeting: args.meeting,
+          eventName: args.eventName,
+          dates: args.dates,
+          resumeType: args.resumeType,
+          executiveId: args.executiveId,
+          preferenceId: args.preferenceId
+        });
+        return event.save();
+      }
+    },
+    addPreference: {
+      type: PreferenceType,
+      args:{
+        allergy:{type:new GraphQLNonNull(GraphQLString)},
+        amenity: {type:GraphQLString},
+        favorites: {type:GraphQLString},
+        executiveId: {type:GraphQLID},
+        eventId:{type:GraphQLID}
+      },
+      resolve(parent, args){
+        let preference = new Preference({
+          allergy: args.allergy,
+          amenity: args.amenity,
+          favorites: args.favorites,
+          executiveId: args.executiveId,
+          eventId: args.eventId
+        });
+        return preference.save();
+      }
     }
   }
 })
 
+
 module.exports = new GraphQLSchema({
-  query:RootQuery
+  query:RootQuery,
+  mutation: Mutation
 });
+
+/*
+//dummy data
+var executives = [
+  {name:'Mary Barra', title:'CEO', preferences:'Allergic to gluten', currentEvent:'4', id:'1', preferenceId:'1', eventId:'1'},
+  {name:'Wrigley Payano', title:'CEO', preferences:'Loves carrots and kills lizards', currentEvent:'4', id:'2', preferenceId:'2', eventId:'2'},
+  {name:'Elon Musk', title:'CFO', preferences:'Loves Pinot Noir from Beune', currentEvent:'5', id:'3', preferenceId:'3', eventId:'3'},
+];
+
+var preferences = [
+  {allergy:"Nuts", amenity:"Sliced fruit and Lime La Croix", favorites: "Apples", executiveId:1", eventId:"1", id:'1'},
+  {allergy:"None", amenity:'Carrots and Hard Boiled Eggs', favorites:'Killing lizards', id:'2', executiveId:'2', eventId:'2'},
+  {allergy:"Gluten", amenity:'Kombucha Ginger and Nuts', favorites:'Swimming', id:'3', executiveId:'3', eventId:'3'},
+];
+
+var events = [
+  {meeting:'Cengage', eventName:'Engage', dates:'Sunday December 12, 2018 through Thursday December 16, 2018', resumeType:'',id:'1', executiveId: '1'},
+  {meeting:'Cisco', eventName:'Cisco Live', dates:'Sunday October 12, 2018 through Thursday October 16, 2018', resumeType:'',id:'2', executiveId:'3'},
+  {meeting:'MicroSoft', eventName:'Azure Live', dates:'Sunday November 12, 2018 through Thursday November 16, 2018', resumeType:'',id:'3', executiveId:'1'},
+  {meeting:'Apple', eventName:'Developer Conference', dates:'Sunday March 5, 2018 through Thursday March 10, 2018', resumeType:'',id:'3', executiveId:'2'},
+  {meeting:'General Motors', eventName:'Cadillac Launch', dates:'Sunday January 9, 2019 through Thursday January 16, 2019', resumeType:'',id:'3', executiveId:'1'},
+  {meeting:'Tesla', eventName:'Model X Launch', dates:'Sunday June 9, 2019 through Thursday June 16, 2019', resumeType:'',id:'3', executiveId:'3'}
+];
+*/
