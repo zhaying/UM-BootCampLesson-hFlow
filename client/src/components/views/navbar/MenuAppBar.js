1a75fb4be5960002445fea7e388afd3701403fad
Lauren Ventura <laurenfventura@gmail.com>
2018-07-14 06:22:38 -0400

    changed logo to be round for avatar

--- client/src/components/views/navbar/MenuAppBar.js
+++ client/src/components/views/navbar/MenuAppBar.js
@@ -9,7 +9,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import logo from '../../../img/atriadwhite.png';
+import logo from '../../../img/atriad4.png';
 //import IconButton from '@material-ui/core/IconButton';
 //import MenuIcon from '@material-ui/icons/Menu';
 //import { grey } from '@material-ui/core/colors';
